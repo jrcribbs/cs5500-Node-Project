615e2f8550d3abbdb3f2ccda61ddab835140a22f
JR Cribbs <jrd.cribbs@gmail.com>
2022-10-08 21:21:57 -0400

    updated with tuits and user

--- server.ts
+++ server.ts
@@ -2,10 +2,34 @@
  * @file Implements an Express Node HTTP server.
  */
 import express, {Request, Response} from 'express';
+import mongoose from "mongoose";
+import UserDao from "./daos/UserDao";
+import UserController from "./controllers/UserController";
+import TuitDao from "./daos/TuitDao";
+import TuitController from "./controllers/TuitController";
+
 const cors = require('cors')
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  autoIndex: false,
+  maxPoolSize: 10,
+  serverSelectionTimeoutMS: 5000,
+  socketTimeoutMS: 45000,
+  family: 4
+}
+
+mongoose.connect('mongodb://localhost:27017/tuitter', options);
+
+
+const userDao = new UserDao();
+const userController = new UserController(app, userDao);
+const tuitDao = new TuitDao();
+const tuitController = new TuitController(app, tuitDao);
 
 app.get('/', (req: Request, res: Response) =>
     res.send('Welcome to Foundation of Software Engineering!!!!'));
@@ -19,3 +43,4 @@ app.get('/hello', (req: Request, res: Response) =>
  */
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
+
