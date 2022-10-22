88b179bbe63a4a765919367797bcfbd29f25cd66
JR Cribbs <jrd.cribbs@gmail.com>
2022-10-22 16:42:19 -0400

    server updates

--- server.ts
+++ server.ts
@@ -2,23 +2,22 @@
  * @file Implements an Express Node HTTP server.
  */
 
-import LikesController
-    from "./likes/LikesController";
-
-
+import LikesController from "./likes/LikesController";
 import TuitDao from "./tuits/TuitDao";
-import TuitController
-    from "./tuits/TuitController";
-
+import TuitController from "./tuits/TuitController";
 import UserDao from "./users/UserDao";
 import UserController from "./users/UserController";
 import express, {Request, Response} from 'express';
 import * as mongoose from "mongoose";
+import BookmarksController from "./bookmarks/BookmarksController";
+import FollowsController from "./follows/FollowsController";
+import MessagesController from "./messages/MessagesController";
+
 const cors = require('cors')
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-
 
 const options = {
     useNewUrlParser: true,
@@ -40,9 +39,6 @@ const dbCallback = (movies: any) => {
     console.log(movies);
 }
 
-// moviesDao
-//     .findAllMovies()
-//     .then(movies => console.log(movies));
 
 function sayHello (req: Request, res: Response) {
     res.send('Hi from FSD 1!!!');
@@ -59,10 +55,14 @@ app.get('/hello', sayHello2);
 const tuitDao = TuitDao.getInstance();
 const tuitController = TuitController.getInstance(app, tuitDao);
 
+LikesController(app);
+BookmarksController(app);
+FollowsController(app);
+MessagesController(app);
+
 /**
  * Start a server listening at port 4000 locally
  * but use environment variable PORT on Heroku if available.
  */
-LikesController(app);
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
